5c1e29b6d4645c6f2eb8e14b1726143a417a0096
Brian Ketelsen <mail@bjk.fyi>
2020-05-08 14:52:05 +0000

    add cosmos maybe

--- api/page-get/index.js
+++ api/page-get/index.js
@@ -1,12 +1,29 @@
+ const CosmosClient = require('@azure/cosmos').CosmosClient
+
+
 module.exports = function (context, req, page) {
-    context.log('JavaScript queue trigger function processed work item');
-    if (!page)
+    const dbResponse = await CosmosClient.databases.createIfNotExists({
+      id: "lifehub"
+    })
+    const database = dbResponse.database
+    const coResponse = await database.containers.createIfNotExists({
+      id: "pages"
+    })
+    container = coResponse.container
+
+    const querySpec = {
+        query: "SELECT * FROM pages p"
+      };
+    const { resources } = await container.items.query(querySpec).fetchAll()
+
+    debug('Setting up the container...done!')
+    if (!resources)
     {
         context.log("Page item not found");
     }
     else
     {
-        context.log("Found page , Slug=" + page.slug);
+      context.res.status(200).json({resources});
     }
 
     context.done();
