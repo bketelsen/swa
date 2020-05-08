e880ef070b69cc9067c3141e229d5e9ffefa0f29
Brian Ketelsen <mail@bjk.fyi>
2020-05-08 11:01:41 +0000

    try table storage

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -1,10 +1,12 @@
-const data = require('../shared/product-data');
+let azure = require('azure-storage');
 
 module.exports = async function (context, req) {
-  try {
-    const products = data.getProducts();
-    context.res.status(200).json(products);
-  } catch (error) {
-    context.res.status(500).send(error);
-  }
+
+    let connectionString = process.env.StorageConnectionString;
+    let tableService = azure.createTableService(connectionString);
+    tableService.queryEntities('pages',null,(error, result, response) => {
+      var pages = result.entities;
+        context.res.status(200).json(pages);
+    });
+
 };
